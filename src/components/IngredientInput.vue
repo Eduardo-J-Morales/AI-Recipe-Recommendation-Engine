e2339453b256bfb7f4c7fc16998650051c8768c0
Eduard-J-Morales <eduardojosegonzalezmorales41@gmail.com>
2025-05-11 14:58:21 -0400

    Updated src/components/IngredientsInput.vue

--- src/components/IngredientInput.vue
+++ src/components/IngredientInput.vue
@@ -2,9 +2,17 @@
     <h2>Enter Ingredients</h2>
     <input v-model="ingredient" type="text" placeholder="Add an ingredient">
     <button @click="addIngredient">Add</button>
+    <ul>
+        <li v-for="item in ingredients" :key="item">{{ item }}</li>
+    </ul>
 </template>
 
 <script>
 import { ref } from 'vue'
+import { useStore } from 'vuex'
+
+const ingredient = ref('')
+const store = useStore()
+const ingredients = store.state.ingredients
 
 </script>
