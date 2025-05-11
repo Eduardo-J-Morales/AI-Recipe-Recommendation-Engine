ac0dd382365169577aa2c7b63b48f11fedd12ec0
Eduard-J-Morales <eduardojosegonzalezmorales41@gmail.com>
2025-05-11 14:34:29 -0400

    Updated src/components/IngredientInput.vue

--- src/components/IngredientInput.vue
+++ src/components/IngredientInput.vue
@@ -1,3 +1,10 @@
 <template>
-    
+    <h2>Enter Ingredients</h2>
+    <input v-model="ingredient" type="text" placeholder="Add an ingredient">
+    <button @click="addIngredient">Add</button>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+</script>
