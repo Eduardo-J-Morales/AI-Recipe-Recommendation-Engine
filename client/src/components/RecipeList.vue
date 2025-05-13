dc73b229f04946aca0bb00453a67cedeeeadcf14
Eduard-J-Morales <eduardojosegonzalezmorales41@gmail.com>
2025-05-13 13:44:12 -0400

    Updated client/src/components/RecipeList.vue

--- client/src/components/RecipeList.vue
+++ client/src/components/RecipeList.vue
@@ -1,6 +1,42 @@
 <template>
     <div>
-        <h2>Recommended Recipes</h2>
-        <ul></ul>
+        <div class="recipe-grid">
+            <div v-for="recipe in store.recipes" :key="recipe" class="recipe-card">
+                // images features
+                <img src="" alt="" class="recipe-image">
+                <div class="recipe-content">
+                    <h3>{{ /*recipe name */ 1}}</h3>
+                    <p class="description"></p>
+                    <div class="recipe=details">
+                        <div class="detail">
+                            <span class="label"></span>
+                            <span></span>
+                        </div>
+                        <div class="detail">
+                            <span class="label"></span>
+                            <span></span>
+                        </div>
+                        <div class="detail">
+                            <span class="label"></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <div class="topics">
+                        <span>
+
+                        </span>
+                    </div>
+                    <button @click="showRecipeDetails(recipe)">
+                        View Details
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+<script setup>
+import { useStore } from '../stores/store';
+
+const store = useStore()
+
+</script>
